cc3a89fd6e5effc6f115a5ffeadf7949e99c2129
nicolau-np <mr1Normaliii@gmail.com>
2023-09-25 16:23:40 +0100

    trabalhando no carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,3 +1,27 @@
-const caroucelItems = document.querySelectorAll(".hero .carousel .item")
+const caroucelItems = document.querySelectorAll(".hero .carousel .item");
 
-console.log(caroucelItems);
+var active = 0;
+var itemsCount = caroucelItems.length - 1;
+
+showCaroucel();
+
+function showCaroucel() {
+  caroucelItems.forEach(element => {
+    element.classList.remove("active");
+  });
+
+  caroucelItems[active].classList.add("active");
+}
+
+function nextCaroucelItem() {
+  if (active == itemsCount) {
+    active = 0;
+  } else {
+    active += 1;
+  }
+  showCaroucel();
+}
+
+setInterval(() => {
+  nextCaroucelItem();
+}, 5000);
